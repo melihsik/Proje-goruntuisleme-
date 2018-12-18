ff12414201f2cace58bde3b682357df26a5c2c64
melihsik <melih0010@gmail.com>
2018-12-18 21:26:56 +0300

    Proje Yüklemesi

---
+++ Mavi Nesne2/Mavi Nesne/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿using System;
+
+namespace Mavi_Nesne
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.proje2gecis = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 65);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(488, 21);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(38, 179);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 33);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1248, 21);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(660, 404);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(506, 504);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(430, 90);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(506, 621);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(430, 90);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(506, 737);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(430, 90);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 504);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 621);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(428, 737);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "B";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(38, 429);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(260, 33);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 294);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 77);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 294);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 77);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 65);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 77);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 237);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Arduino(Port)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 21);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 25);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Camera";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1227, 710);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(193, 82);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // proje2gecis
+            // 
+            this.proje2gecis.Location = new System.Drawing.Point(1227, 582);
+            this.proje2gecis.Name = "proje2gecis";
+            this.proje2gecis.Size = new System.Drawing.Size(193, 93);
+            this.proje2gecis.TabIndex = 18;
+            this.proje2gecis.Text = "Proje2";
+            this.proje2gecis.UseVisualStyleBackColor = true;
+            this.proje2gecis.Click += new System.EventHandler(this.proje2gecis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1984, 844);
+            this.Controls.Add(this.proje2gecis);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button proje2gecis;
+    }
+}
+
---
+++ Mavi Nesne2/Mavi Nesne/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge;
+using AForge.Math.Geometry;
+using System.IO.Ports;
+
+namespace Mavi_Nesne
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice kamera;
+        private FilterInfoCollection cihazlar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int R;
+        int G;
+        int B;
+        int pin;
+        int proje = 1;
+        int state;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.DataSource = SerialPort.GetPortNames();
+            cihazlar = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo cihaz in cihazlar)
+            {
+                comboBox1.Items.Add(cihaz.Name);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+                kamera = new VideoCaptureDevice(cihazlar[comboBox1.SelectedIndex].MonikerString);
+                kamera.NewFrame += new NewFrameEventHandler (kamera_NewFrame);
+                kamera.Start();
+        }
+        private void kamera_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+            image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            image1.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox1.Image = image;
+            EuclideanColorFiltering filter = new EuclideanColorFiltering();
+            filter.CenterColor = new RGB(Color.FromArgb(R, G, B)); 
+            filter.Radius = 100;
+            filter.ApplyInPlace(image1);        
+            cevreal(image1);
+        }
+        private void cevreal(Bitmap image)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.MinWidth = 2;
+            blobCounter.MinHeight = 2;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+            Grayscale grayFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap grayImage = grayFilter.Apply(image);
+            blobCounter.ProcessImage(grayImage);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            
+            foreach (Rectangle recs in rects)
+            {
+                if (rects.Length > 0)
+                {
+                    Rectangle objectRect = rects[0];
+                    Graphics g = Graphics.FromImage(image);
+                    using (Pen pen = new Pen(Color.FromArgb(252, 3, 26), 2))
+                    {
+                        g.DrawRectangle(pen, objectRect);
+                    }
+                    int objectX = objectRect.X + (objectRect.Width / 2); //Dikdörtgenin Koordinatlari alınır.
+                    int objectY = objectRect.Y + (objectRect.Height / 2);//Dikdörtgenin Koordinatlari alınır.
+                    g.DrawString(objectX.ToString() + "X" + objectY.ToString() + "Y", new Font("Arial", 12), Brushes.Red, new System.Drawing.Point(100, 2));
+                    g.Dispose();
+                    if (proje == 1)
+                    {
+                        if (objectX > 0 && objectX < 106 && objectY < 80)
+                        {
+                            pin = 1;
+                        }
+                        if (objectX > 106 && objectX < 212 && objectY < 80)
+                        {
+                            pin = 2;
+                        }
+                        if (objectX > 212 && objectX < 320 && objectY < 80)
+                        {
+                            pin = 3;
+                        }
+                        if (objectX > 0 && objectX < 106 && objectY > 80 && objectY < 160)
+                        {
+                            pin = 4;
+                        }
+                        if (objectX > 106 && objectX < 212 && objectY > 80 && objectY < 160)
+                        {
+                            pin = 5;
+                        }
+                        if (objectX > 212 && objectX < 320 && objectY > 80 && objectY < 160)
+                        {
+                            pin = 6;
+                        }
+                        if (objectX > 0 && objectX < 106 && objectY > 160 && objectY < 240)
+                        {
+                            pin = 7;
+                        }
+                        if (objectX > 106 && objectX < 212 && objectY > 160 && objectY < 240)
+                        {
+                            pin = 8;
+                        }
+                        if (objectX > 212 && objectX < 320 && objectY > 160 && objectY < 240)
+                        {
+                            pin = 9;
+                        }
+                        if (serialPort1.IsOpen)
+                        {
+                            serialPort1.Write(pin.ToString());
+                        }
+                    }
+                    else if (proje==2)
+                    {
+                        if (objectX > 0 && objectX <= 130 )
+                        {
+                            state = 1;
+                        }
+                        else if (objectX > 130 && objectX <=190)
+                        {
+                            state = 2;
+                        }
+                        else if (objectX > 190 && objectX <= 320)
+                        {
+                            state = 3;
+                        }
+
+                    }
+                    if (serialPort1.IsOpen)
+                    {
+                        serialPort1.Write(state.ToString());
+                    }
+                    
+                }
+            }
+            pictureBox2.Image = image;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (kamera.IsRunning)
+                kamera.Stop();
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            R = trackBar1.Value;
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            G = trackBar2.Value;
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            B = trackBar3.Value;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = comboBox2.SelectedItem.ToString();
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+               MessageBox.Show("Port Açık");
+           }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            MessageBox.Show("Port Kapalı");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (kamera.IsRunning)
+            {
+                kamera.Stop(); 
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Programı Kapatmak İstiyor Musunuz ?", "Dikkat",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+                Application.Exit();
+            }
+            else
+            {
+            }
+    }
+
+        private void proje2gecis_Click(object sender, EventArgs e)
+        {
+            if (proje==1)
+            {
+                proje = 2;
+                proje2gecis.BackColor = Color.FromArgb(255, 0, 0);
+                proje2gecis.ForeColor = Color.White;
+            }
+            else if (proje==2)
+            {
+                proje = 1;
+                proje2gecis.BackColor = Color.FromArgb(255, 255, 255);
+                proje2gecis.ForeColor = Color.Black;
+            }
+        }
+    }
+}
